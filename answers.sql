3a40c91b4585f863e80b1777cbabce7d6affa0e2
N4tasha-tech <winnienatasha3@gmail.com>
2025-09-17 13:09:15 +0300

    Student table

--- answers.sql
+++ answers.sql
@@ -1,1 +1,26 @@
+CREATE TABLE student (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age INT CHECK (age > 0),      
+    gender VARCHAR(10) NOT NULL
+);
+
+INSERT INTO student (name, age, gender)
+VALUES ('Mark Mariko', 19, 'Male');
+
+INSERT INTO student (name, age, gender)
+VALUES ('Norah Machego', 24, 'Female');
+
+INSERT INTO student (name, age, gender)
+VALUES ('Isabella Rossey', 22, 'Female');
+
+INSERT INTO student (name, age, gender)
+VALUES ('Chloe Wanjiku', 19, 'Female');
+
+INSERT INTO student (name, age, gender)
+VALUES ('Daniel Mareba', 21, 'Male');
+
+INSERT INTO student (name, age, gender)
+VALUES ('Eva Kangola', 23, 'Female');
+
 
